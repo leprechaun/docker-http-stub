3b94239dd9d3983013821b40c361dc6cd82725a5
Laurence MacGuire <leprechaun@gmail.com>
2016-10-24 17:42:34 +0800

    add support for mime types

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask import request
+from flask import make_response
 import time
 
 app = Flask(__name__)
 
-@app.route('/<int:status>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'])
-def stub(status):
+@app.route('/<base>/<type>/<int:status>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'])
+def stub(base, type, status):
     wait = int(request.args.get('wait', '0'))
     time.sleep(int(wait))
 
-    return 'Hello, World!', status
+    response_text = "Hello world"
+    response = make_response(response_text, status)
+    response.headers["mime-type"] = "/".join([base, type])
+
+    return response
